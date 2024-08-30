bc26bd903eb2990bbbf974866aedaec9284a49a9
viceam <m99viceam@outlook.com>
2024-08-30 18:32:15 +0800

    add pojo

--- src/main/java/org/thirtysix/talentnexus/pojo/JobSeeker.java
+++ src/main/java/org/thirtysix/talentnexus/pojo/JobSeeker.java
@@ -1,2 +1,24 @@
-package org.thirtysix.talentnexus.pojo;public class JobSeeker {
+package org.thirtysix.talentnexus.pojo;
+
+import lombok.Data;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+public class JobSeeker {
+    private int id;
+    private String username;
+    private String passwordHash;
+    private String email;
+    private String phone;
+    private String fullName;
+    private GenderEnum gender;
+    private LocalDate birthDate;
+    private String address;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
+
+enum GenderEnum {
+    MALE, FEMALE, OTHER
 }
--- src/main/java/org/thirtysix/talentnexus/pojo/ProjectExperience.java
+++ src/main/java/org/thirtysix/talentnexus/pojo/ProjectExperience.java
@@ -1,2 +1,16 @@
-package org.thirtysix.talentnexus.pojo;public class ProjectExperience {
+package org.thirtysix.talentnexus.pojo;
+
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class ProjectExperience {
+    private int id;
+    private int resumeId;
+    private String projectName;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String role;
+    private String achievements;
 }
