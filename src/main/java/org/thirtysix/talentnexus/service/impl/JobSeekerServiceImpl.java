83189be30e4193cdd3ad56c895c3e5e961217e40
viceam <m99viceam@outlook.com>
2024-08-31 13:32:08 +0800

    seeker register

--- src/main/java/org/thirtysix/talentnexus/service/impl/JobSeekerServiceImpl.java
+++ src/main/java/org/thirtysix/talentnexus/service/impl/JobSeekerServiceImpl.java
@@ -8,9 +8,12 @@ import org.thirtysix.talentnexus.mapper.JobSeekerMapper;
 import org.thirtysix.talentnexus.pojo.JobSeeker;
 import org.thirtysix.talentnexus.service.JobSeekerService;
 import org.thirtysix.talentnexus.utl.PasswordUtil;
+import org.apache.log4j.Logger;
 
 @Service
 public class JobSeekerServiceImpl implements JobSeekerService {
+    private static final Logger LOGGER = Logger.getLogger(JobSeekerServiceImpl.class);
+
     @Autowired
     JobSeekerMapper jobSeekerMapper;
 
@@ -53,6 +56,7 @@ public class JobSeekerServiceImpl implements JobSeekerService {
         try {
             jobSeekerMapper.insertJobSeeker(seeker);
         } catch (Exception e) {
+            LOGGER.error("Error occurred during registration", e);
             return "err";
         }
         return "ok";
