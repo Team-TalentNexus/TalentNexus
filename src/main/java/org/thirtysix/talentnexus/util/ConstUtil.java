5af68f499812a13fb18623833dd861286ad96c7d
viceam <m99viceam@outlook.com>
2024-09-01 19:08:51 +0800

    修改公司命名

--- src/main/java/org/thirtysix/talentnexus/util/ConstUtil.java
+++ src/main/java/org/thirtysix/talentnexus/util/ConstUtil.java
@@ -2,6 +2,6 @@ package org.thirtysix.talentnexus.util;
 
 public class ConstUtil {
     public static final String SEEKER = "Seeker";
-    public static final String ENTERPRISE = "Enterprise";
+    public static final String COMPANY = "Company";
     public static final String ADMIN = "Admin";
 }
